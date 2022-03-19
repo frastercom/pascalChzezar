ddfc2d5e273370e8e26ed7cab68f2c7eef00a54e
frastercom <37472618+frastercom@users.noreply.github.com>
2022-03-19 09:49:28 +0300

    Add units 6 and 7

--- Pascal.pas
+++ Pascal.pas
@@ -137,3 +137,68 @@ begin
   writeln(s1);
 end.
 
+
+Задание 6.
+
+program shifr1;
+const s='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+var x,h:integer; s1:string;
+begin
+  writeln('введите строку');
+  readln(s1);
+  writeln('введите шифр в виде цифры');
+  readln(x);
+  for var i:=1 to s1.Length do
+    for var i1:=1 to s.Length do
+      if s[i1] = s1[i] then
+      begin
+          if x>=0 then
+          begin
+            if i1+x<=s.Length then
+            begin
+              s1[i] := s[i1+x];
+              break;
+            end
+            else
+            begin
+              h:= i1+x-s.Length;
+              while h>s.Length do
+                h:= h-s.Length;
+              s1[i] := s[h];
+            end
+          end
+          else
+          begin
+            if i1+x>1 then
+            begin
+              s1[i] := s[i1+x];
+              break;
+            end
+            else
+            begin
+              h:= i1+x+s.Length;
+              while h<1 do
+                h:= h+s.Length;
+              s1[i] := s[h];
+            end
+          end;
+      end;
+  writeln('зашифрованная строка');
+  writeln(s1);
+end.
+
+Задание 7.
+
+program shifr;
+var i:integer; s:string;
+begin
+  writeln('введите строку');
+  readln(s);
+
+  writeln('введите шифр в виде цифры');
+  readln(x);
+
+  for i:=1 to s.Length do
+      s[i]:=chr(ord(s[i])-x);
+  writeln(s);
+end.
